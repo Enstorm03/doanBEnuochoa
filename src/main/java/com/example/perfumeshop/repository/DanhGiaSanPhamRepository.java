2cb4531b6c082530ea3b56d82505ba27623ab5f2
Tuyen <tuyen88931600@gmail.com>
2026-04-16 10:34:19 +0700

    haaaa

--- src/main/java/com/example/perfumeshop/repository/DanhGiaSanPhamRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.perfumeshop.repository;
-
-import com.example.perfumeshop.entity.DanhGiaSanPham;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DanhGiaSanPhamRepository extends JpaRepository<DanhGiaSanPham, Integer> {
-}
